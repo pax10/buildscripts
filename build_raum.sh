d10fb3b05c965f07432aee69ed0bb282ae9dbc84
Ty Pobe <pax10@mobileinfern.us>
2013-07-30 09:47:21 -0700

    edits to raum bs
    ..
    
     Changes to be committed:
    	modified:   build_raum.sh

--- build_raum.sh
+++ build_raum.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 # Colorize and add text parameters
-red=$(tput setaf 1)             #  red
-grn=$(tput setaf 2)             #  green
-cya=$(tput setaf 6)             #  cyan
-txtbld=$(tput bold)             # Bold
-bldred=${txtbld}$(tput setaf 1) #  red
-bldgrn=${txtbld}$(tput setaf 2) #  green
-bldblu=${txtbld}$(tput setaf 4) #  blue
-bldcya=${txtbld}$(tput setaf 6) #  cyan
-txtrst=$(tput sgr0)             # Reset
+red=$(tput setaf 1) # red
+grn=$(tput setaf 2) # green
+cya=$(tput setaf 6) # cyan
+txtbld=$(tput bold) # Bold
+bldred=${txtbld}$(tput setaf 1) # red
+bldgrn=${txtbld}$(tput setaf 2) # green
+bldblu=${txtbld}$(tput setaf 4) # blue
+bldcya=${txtbld}$(tput setaf 6) # cyan
+txtrst=$(tput sgr0) # Reset
 
 DEVICE="$1"
-SYNC="$2"
-THREADS="$3"
-CLEAN="$4"
-
+THREADS="$2"
+NICE="$3"
+SYNC="$4"
+CLEAN="$5"
 
 # Build Date/Version
 VERSION=`date +%Y%m%d`
 
 # Directory where you wanna store build logs
-LOGS="$HOME/android/build_logs"
+LOGS="../build_logs"
+
+SYSPATH="${PWD}/out/target/product/toro/system"
 
 # Time of build startup
 res1=$(date +%s.%N)
@@ -36,22 +38,28 @@ echo -e
 
 # sync with latest sources
 echo -e ""
-if [ "$SYNC" == "sync" ]
-then
-   echo -e "${bldred}Syncing latest linaro+raumZero sources ${txtrst}"
+if [ "$SYNC" == "sync" ]; then
+echo -e "${bldred}Syncing latest raumZero manifest ${txtrst}"
    repo sync -j"$THREADS"
    echo -e ""
 fi
 
 # setup environment
-if [ "$CLEAN" == "clean" ]
-then
-   echo -e "${bldred}Cleaning up out folder ${txtrst}"
+if [ "$CLEAN" == "clean" ]; then
+echo -e "${bldred}Cleaning up out folder ${txtrst}"
    make clobber;
 else
-  echo -e "${bldred}Skipping out folder cleanup ${txtrst}"
+echo -e "${bldred}Skipping out folder cleanup ${txtrst}"
 fi
 
+# use nice
+if [ "$NICE" == "" ]; then
+NICE="10"
+elif [ "$NICE" == "0" ]; then
+NICE="19"
+else
+NICE="$NICE"
+fi
 
 # setup environment
 echo -e "${bldred}Setting up build environment ${txtrst}"
@@ -60,15 +68,25 @@ echo -e "${bldred}Setting up build environment ${txtrst}"
 # lunch device
 echo -e ""
 echo -e "${bldred}Lunching your device ${txtrst}"
-lunch "raumzero_$DEVICE-userdebug";
+lunch "full_$DEVICE-userdebug";
 
 echo -e ""
+echo -e "Creating working around symlink for Arch Linux lib usr/lib"
+mkdir -p $SYSPATH/lib
+mkdir -p $SYSPATH/usr
+ln -s $SYSPATH/lib/ $SYSPATH/usr/lib
+
+echo -e "Symlinked dirs created... "
+
+echo " "
 
 echo -e "${bldred}Starting raumZero build for $DEVICE ${txtrst}"
 
+echo
+echo "device=$DEVICE nice=$NICE threads=$THREADS "
 # start compilation
 # log builds by date + time
-time brunch "raumzero_$DEVICE-userdebug" -j"$THREADS" >$LOGS/raumzero_$DEVICE-userdebug-$(date +'%Y%m%d-%T').log 2>&1;
+time nice -n"$NICE" make otapackage -j"$THREADS" 2>&1 | tee $LOGS/raumzero_$DEVICE.log;
 echo -e ""
 
 # finished? get elapsed time
